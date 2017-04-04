22ab627b46d3516689250fadc372041c57c11461
dylanito2 <dylanito2@gmail.com>
2017-04-04 16:54:35 -0400

    Done.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,13 +4,24 @@ const React = require('react');
 const SidebarItem = require('./SidebarItem');
 
 class Sidebar extends React.Component {
+
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
   handleClick(index, ev) {
+    ev.preventDefault()
+    this.props.onSelect(index)
   }
   render() {
     const { files, selectedFileIndex, onAdd } = this.props;
+    const items = files.map((file, i) => {
+      return <SidebarItem file={file} key={i} isSelected={i === selectedFileIndex} onClick={this.handleClick.bind(null, i)} />
+    })
 
     return (
       <ul className="sidebar">
+        {items}
       </ul>
     );
   }
--- stores/fileStore.js
+++ stores/fileStore.js
@@ -2,7 +2,31 @@
 
 const Store = require('./Store');
 
-class FileStore {
+class FileStore extends Store {
+  constructor(files=['']) {
+    super(files)
+  }
+
+  addFile() {
+    let files = [...this.getState(), '']
+    this.setState(files)
+  }
+
+  removeFile(index) {
+    let files = [...this.getState()]
+    if (files.length > 1) {
+      files.splice(index, 1)
+    } else {
+      files = ['']
+    }
+    this.setState(files)
+  }
+
+  updateFile(index, file) {
+    let files = [...this.getState()]
+    files[index] = file
+    this.setState(files)
+  }
 }
 
-module.exports = new FileStore([]);
+module.exports = new FileStore();
