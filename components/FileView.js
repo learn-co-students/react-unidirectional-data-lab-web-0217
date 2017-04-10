18da28a60a42c45df13795a69e4caa1880b93bd5
James Phan <jamesvphan@gmail.com>
2017-04-10 15:28:18 -0400

    Done.

--- components/FileView.js
+++ components/FileView.js
@@ -1,13 +1,17 @@
 'use strict'
 
-import React from 'react';
-import Toolbar from './Toolbar';
+const React = require('react');
+const Toolbar = require('./Toolbar');
 
-export default class FileView extends React.Component {
+class FileView extends React.Component {
   render() {
     return (
-      <div>
+      <div className='file-view'>
+        <Toolbar onAdd={this.props.onAdd} onRemove={this.props.onRemove} />
+        <textarea className='file-view__text' value={this.props.file} onChange={this.props.onChange} />
       </div>
-    );
+    )
   }
 }
+
+module.exports = FileView
